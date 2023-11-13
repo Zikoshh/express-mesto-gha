7b6bea4fd4b2d257b6aa79fb69f24efc85b5a3ad
decideR <chimasterchimaster@gmail.com>
2023-11-13 18:04:02 +0700

    Решил проблему с неуправляемыми роутами

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => {
+  res.status(404).send({ message: 'Мы не обрабатываем данный роут' });
+});
 
 module.exports = router;
