d62022480eac0382aca8dd9fa1156f1beb2d47ca
decideR <chimasterchimaster@gmail.com>
2023-11-13 15:57:21 +0700

    Перенес middleware в app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
-const { hardCodingAId } = require('./middlewares/hardCodingAId');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
-app.use(hardCodingAId);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6551cda6b3fed233a14d8a7e',
+  };
+
+  next();
+});
 app.use(router);
 app.listen(PORT);
